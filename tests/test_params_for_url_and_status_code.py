f80063521d43814e66cb6e1ec86334d1901ed257
Sergei <s.vasilchenko@busfor.com>
2020-02-05 18:23:11 +0300

    add small changes

--- tests/test_params_for_url_and_status_code.py
+++ tests/test_params_for_url_and_status_code.py
@@ -5,8 +5,8 @@ def test_url2(url_param, status_code_param):
     """Если в --url_param передать не ya.ru, то в status code будет 404"""
     if url_param == 'https://ya.ru':
         status_code_param = '200'
-        print(status_code_param)
+        print(url_param, status_code_param)
     else:
         status_code_param = '404'
-        print(status_code_param)
+        print(url_param, status_code_param)
         #assert False
