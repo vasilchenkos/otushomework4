419a224f335fdabb76ad48ef5c2fe4c80bfbe9ad
Sergei <s.vasilchenko@busfor.com>
2020-02-10 19:06:49 +0300

    add tests for openbrewerydb

--- tests/dog_api_test.py
+++ tests/dog_api_test.py
@@ -1,24 +1,22 @@
 import requests
 import pytest
-
-# можем менять данные для ожидаемого результата
-TESTDATABREEDS = [
-    (['afghan', 'basset', 'blood', 'english', 'ibizan', 'plott', 'walker'])
-    ]
+from helpers import fixed_data
 
 
-@pytest.mark.parametrize("expected", TESTDATABREEDS)
+@pytest.mark.parametrize("expected", fixed_data.TESTDATABREEDS)
 def test_body(expected, url_param='https://dog.ceo/api/breed/hound/list'):
     """Проверяем совпадает ли ожидаемый месседж"""
     response = requests.get(url_param)
     json_dict = response.json()['message']
     assert json_dict == expected
 
+
 def test_status(url_param='https://dog.ceo/api/breed/hound/afghan/images/random'):
-    """Проверяем что статус в месседже success"""
+    """Проверяем статус в месседже success"""
     response = requests.get(url_param)
     status = response.json()['status']
     assert status == 'success'
+
 
 def test_list_count_breeds(url_param='https://dog.ceo/api/breeds/list/all'):
     """Проверяем количество пород"""
@@ -26,19 +24,21 @@ def test_list_count_breeds(url_param='https://dog.ceo/api/breeds/list/all'):
     dict_breeds = response.json()['message']
     assert len(dict_breeds) == 92
 
+
 @pytest.mark.parametrize("test_breeds, expected", [('waterdog', ['spanish']),
                                                    ('wolfhound', ['irish']),
                                                    ('whippet', []),
-                                                    ('terrier', ['american', 'australian', 'bedlington', 'border',
-                                                    'dandie', 'fox', 'irish', 'kerryblue',
-                                                    'lakeland', 'norfolk', 'norwich', 'patterdale',
-                                                    'russell', 'scottish', 'sealyham', 'silky', 'tibetan',
-                                                    'toy', 'westhighland', 'wheaten', 'yorkshire'])])
+                                                   ('terrier', ['american', 'australian', 'bedlington', 'border',
+                                                                'dandie', 'fox', 'irish', 'kerryblue',
+                                                                'lakeland', 'norfolk', 'norwich', 'patterdale',
+                                                                'russell', 'scottish', 'sealyham', 'silky', 'tibetan',
+                                                                'toy', 'westhighland', 'wheaten', 'yorkshire'])])
 def test_get_breed(test_breeds, expected, url_param='https://dog.ceo/api/breeds/list/all'):
     """Проверяем наличие породы"""
     response = requests.get(url_param)
     dict_breeds = response.json()['message']
     assert dict_breeds.get(test_breeds) == expected
+
 
 def test_add_breed(url_param='https://dog.ceo/api/breeds/list/all'):
     """Проверяем наличие породы после добавления"""
